710df9422c822f4d5f5312c772ddff1e65da47e8
Uros Bajzelj <ubajze@users.noreply.github.com>
2026-01-13 10:27:40 +0100

    add the command to check the release tag

--- tasks.py
+++ tasks.py
@@ -2,6 +2,9 @@
 (c) 2022 Network To Code
 """
 
+import yaml
+from pathlib import Path
+
 from invoke import task
 
 
@@ -13,3 +16,23 @@ def docs(context, strict=False):
         command = f"{command} --strict"
     print("Serving Documentation...")
     context.run(command)
+
+
+@task
+def check_release_tag(context, tag):
+    """Check that the provided tag matches the value in Chart.yaml."""
+
+    print(f"Checking that provided tag '{tag}' matches Chart.yaml version.")
+
+    print("Loading Chart.yaml.")
+    chart_yaml_path = Path("charts/nautobot/Chart.yaml")
+    with open(chart_yaml_path, "r", encoding="utf-8") as chart_file:
+        chart_yaml_data = yaml.safe_load(chart_file)
+        version = chart_yaml_data.get("version")
+        print(f"Found version '{version}' in Chart.yaml.")
+
+    if tag != version:
+        print(f"ERROR: Provided tag '{tag}' does not match Chart.yaml version '{version}'.")
+        exit(1)
+
+    print(f"Provided tag '{tag}' matches Chart.yaml version '{version}'.")
